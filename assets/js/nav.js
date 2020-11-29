1acaff4a25e9d4d47f9bea0cfa409808b1dcc323
dono44 <75192313+dono44@users.noreply.github.com>
2020-11-29 20:40:01 +0100

    update nav.js

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -3,10 +3,10 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "90px 10px";
+    document.getElementById("navbar").style.padding = "30px 10px";
     document.getElementById("logo").style.fontSize = "10px";
   } else {
-    document.getElementById("navbar").style.padding = "90px 10px";
+    document.getElementById("navbar").style.padding = "30px 10px";
     document.getElementById("logo").style.fontSize = "10px";
   }
 }
