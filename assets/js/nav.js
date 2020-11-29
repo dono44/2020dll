49845681c0d18b02cf04547d80cdb16da92e291b
dono44 <don00765@gmail.com>
2020-11-29 21:08:05 +0100

    modif entete3

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -3,14 +3,14 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "90px 10px";
-    document.getElementById("logo").style.fontSize = "5px";
+    document.getElementById("navbar").style.padding = "1px 10px";
+    document.getElementById("logo").style.fontSize = "10px";
 
-    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("navbar").style.padding = "1px 10px";
     document.getElementById("logo").style.fontSize = "10px";
 
   } else {
-    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("navbar").style.padding = "1px 10px";
     document.getElementById("logo").style.fontSize = "10px";
   }
 }
