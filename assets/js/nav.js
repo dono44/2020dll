56c5034d5ab610b6363c3c860b903f00bd27c9fd
dono44 <don00765@gmail.com>
2020-11-29 19:08:40 +0100

    modif page

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -3,10 +3,10 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("navbar").style.padding = "10px 10px";
     document.getElementById("logo").style.fontSize = "5px";
   } else {
-    document.getElementById("navbar").style.padding = "50px 10px";
+    document.getElementById("navbar").style.padding = "30px 10px";
     document.getElementById("logo").style.fontSize = "10px";
   }
 }
